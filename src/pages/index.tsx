98337429503339b1f9422a7f3fae0234d39364f5
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-03-13 12:47:38 -0300

    refactor: removed place array from render

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,5 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return (
-    <Map
-      places={[
-        {
-          id: '1',
-          name: 'Petrópolis',
-          slug: 'petropolis',
-          location: {
-            latitude: 0,
-            longitude: 0
-          }
-        },
-        {
-          id: '2',
-          name: 'São Paulo',
-          slug: 'saopaulo',
-          location: {
-            latitude: 10,
-            longitude: 30
-          }
-        }
-      ]}
-    />
-  )
+  return <Map />
 }
