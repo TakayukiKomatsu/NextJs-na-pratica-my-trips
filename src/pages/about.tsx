83f207485630e9392273212cd262a2fe99f8b4f2
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-03-18 19:41:15 -0300

    feat: add pages query

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,28 @@
+import client from 'graphql/client'
+
 import AboutTemplate from 'template/About'
+import { GET_PAGES } from 'graphql/queries'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  return <AboutTemplate />
+}
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
+
+/*
+- getStaticPaths -server para gerar as urls em build time /about /trip/petropolis
+- getStaticProps - server para buscar dados da página  (props) - build time
+- getServerSideProps - serve para buscar os dados da página (props) - Run Time (bundle fica no server)
+
+// Not Recommended
+- getInitialProps - serve para buscar os dados da página (props) - Run Time (bundle vai para o client) - Hydrate
+
+*/
